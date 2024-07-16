f162de82b908124da80a49da5f3ab9575d662f6c
Abdelrahman Yehia Ibrahim <freeaccforu98@gmail.com>
2024-07-16 16:39:18 +0300

    Update schema.js

--- src/domains/user/schema.js
+++ src/domains/user/schema.js
@@ -1,8 +1,8 @@
 const Joi=require("joi");
 const EditSchema=Joi.object({
     email:Joi.string().email().required(),
-    username:Joi.string().min(3).max(20).required(),
-    password:Joi.string().min(8).max(20).required(),
+    username:Joi.string().min(3).max(20).optional(),
+    password:Joi.string().min(8).max(20).optional(),
     securityQuestion:Joi.string().min(3).max(40).optional(),
     securityAnswer:Joi.string().min(3).max(40).optional(),
 });
